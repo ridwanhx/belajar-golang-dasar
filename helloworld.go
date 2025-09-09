c3edff4c93b52708725ecc66b46b55fc3db8c11d
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2025-09-10 02:23:09 +0700

    Eps.04 - Belajar Go-Lang Dasar : membuat program hello world

---
+++ helloworld.go
@@ -0,0 +1,7 @@
+package main	// package utama, dimana program akan dijalankan dari package ini
+import "fmt"	// mengimport package fmt, yang berisi fungsi untuk melakukan format input dan output
+
+// main function, fungsi utama yang akan dijalankan pertama kali
+func main() {
+	fmt.Println("Hello, World!");	// mencetak tulisan "Hello, World!" ke layar
+}
