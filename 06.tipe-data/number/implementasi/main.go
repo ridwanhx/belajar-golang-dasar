89d0e284d0c50e8b91b9dab14a49f5d0aa095439
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2025-09-13 23:49:19 +0700

    Eps.06 - Belajar Go-Lang Dasar : tipe data number

---
+++ 06.tipe-data/number/implementasi/main.go
@@ -0,0 +1,10 @@
+// # Implementasi materi Tipe data Number
+package main;
+
+import "fmt";
+
+func main() {
+	fmt.Println("Satu = ", 4);	// integer
+	fmt.Println("Dua = ", 14);	// integer
+	fmt.Println("Tiga koma lima = ", 3.5);	// float
+}
